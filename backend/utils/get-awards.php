f50973f26ebee89345e4889659e99aa0de87bf0b
Rakesh Prashant Mal <malrakesh172@gmail.com>
2025-07-02 17:36:35 +0530

    initial commit

--- backend/utils/get-awards.php
+++ backend/utils/get-awards.php
@@ -1,12 +1,27 @@
 <?php
 include '../db.php';
 
-$sql = "SELECT * FROM awards ORDER BY year DESC";
+$type = isset($_GET['type']) ? $_GET['type'] : 'all';
+
+switch ($type) {
+    case 'student':
+        $sql = "SELECT * FROM awards WHERE category = 'Student'";
+        break;
+    case 'faculty':
+        $sql = "SELECT * FROM awards WHERE category = 'Faculty'";
+        break;
+    case 'achievement':
+        $sql = "SELECT * FROM awards WHERE category = 'Achievement'";
+        break;
+    default:
+        $sql = "SELECT * FROM awards";
+}
+
 $result = $conn->query($sql);
 
 $awards = [];
 while ($row = $result->fetch_assoc()) {
-    $row['image'] = $row['image'] ?: 'default.jpg'; // fallback image
+    $row['image'] = $row['image'] ?: 'default.jpg';
     $awards[] = $row;
 }
 
